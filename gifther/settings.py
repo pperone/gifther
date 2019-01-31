330b0f718061735b95a61b0de8465e7984328acb
Pedro Perone <pperone@gmail.com>
2019-01-31 14:29:23 -0300

    Update settings.py

--- gifther/settings.py
+++ gifther/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^fenb5c3y(s%fzzxrs=%01hb3x_48rh1oli8p+d866oe=-ad74'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
